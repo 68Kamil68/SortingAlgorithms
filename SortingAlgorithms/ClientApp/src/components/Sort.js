2c14307a257459338a9c1fcc001212699695f267
Kamil <tret1lus@gmail.com>
2020-02-02 17:31:52 +0100

    added sort component prototype

---
+++ SortingAlgorithms/ClientApp/src/components/Sort.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from "react";
+
+export class Sort extends Component {
+    static displayName = Sort.name;
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Sorting Algorithms</h1>
+
+                <button className="btn btn-primary">Generate unsorted array</button>
+            </div>
+        );
+    }
+}
