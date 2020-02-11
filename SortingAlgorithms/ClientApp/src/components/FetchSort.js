97ee8aa76cdb66a8feacaff814a68a84f3b83028
Kamil Krakowski <tret1lus@gmail.com>
2020-02-11 17:18:11 +0100

    added sort buttons, fetching data from backend working correctly

--- SortingAlgorithms/ClientApp/src/components/FetchSort.js
+++ SortingAlgorithms/ClientApp/src/components/FetchSort.js
@@ -5,42 +5,120 @@ export class FetchSort extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { array: [] };
+        this.state = { array: [], noArrayReceived: true, sorted: false, sorting: true };
         this.unsortedArray = [];
         this.size = 10;
         this.getUnsortedArray = this.getUnsortedArray.bind(this);
     }
-    //TODO: DRAGGABLE SIZE, SHOWING ARRAY
+    //TODO: DRAGGABLE SIZE
+
     render() {
+        let contents = this.state.noArrayReceived
+            ? <p><em>Set size and generate an array</em></p>
+            : FetchSort.renderArray(this.state.array);
+        let sortButtons = this.state.noArrayReceived
+            ? <p>.</p>
+            : <><button className="btn btn-primary" onClick={() => this.bubbleSort()}>Use bubble sort</button>
+            <button className="btn btn-primary" onClick={() => this.selectionSort()}>Use selection sort</button>
+            <button className="btn btn-primary" onClick={() => this.mergeSort()}>Use merge sort</button>
+            <button className="btn btn-primary" onClick={() => this.quickSort()}>Use quick sort</button></>;
+
+        let sortedNotification = this.state.sorted 
+            ? <p><em>sorted</em></p>
+            : <p><em>not sorted</em></p>
+
+        let sortingInfo = this.state.sorting 
+            ? <br></br>
+            : <p><em>sorting...</em></p>
         return (
             <div>
                 <h1>Sorting Algorithms</h1>
                 Size of an array: <input type="number" id="num" defaultValue="100"/>
-                <button className="btn btn-primary" onClick={() => this.getUnsortedArray(11)}>Generate unsorted array</button>
+                <button className="btn btn-primary" onClick={() => this.getUnsortedArray()}>Generate unsorted array</button>
+                {contents}
+                {sortButtons}
+                {sortedNotification}
+                {sortingInfo}
             </div>
         );
     }
 
-    async getUnsortedArray(data) {
-        const response = await fetch('sort', {
+    async getUnsortedArray() {
+        const size = document.getElementById("num").value;
+        const response = await fetch('Sort/Generate', {
             method: 'POST',
             headers: {
-                'Content-Type': 'text/plain',
+                'Content-Type': 'application/json',
             },
-            body: data,
+            body: JSON.stringify(size),
         });
-        //const response = await fetch('sort', {
-        //    method: "POST",
-        //    headers: {
-        //        'Accept': 'application/json',
-        //        'Content-Type': 'application/json',
-        //    },
-        //    body: Size = '10'
-        //});
-        //console.log(size);
         const array = await response.json();
         console.log(array);
+        this.setState({ array: array, noArrayReceived: false, sorted: false });
         this.unsortedArray = array;
-        //this.setState({ array: data });
+    }
+
+    async quickSort() {
+        this.setState({ array: this.unsortedArray, sorted: false, sorting: true });
+        const response = await fetch('Sort/QuickSort', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.unsortedArray),
+        });
+        const array = await response.json();
+        this.setState({ array: array, sorted: true, sorting: false });
+    }
+
+    async mergeSort() {
+        this.setState({ array: this.unsortedArray, sorted: false, sorting: true });
+        const response = await fetch('Sort/MergeSort', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.unsortedArray),
+        });
+        const array = await response.json();
+        this.setState({ array: array, sorted: true, sorting: false });
+    }
+
+    async bubbleSort() {
+        this.setState({ array: this.unsortedArray, sorted: false, sorting: true });
+        const response = await fetch('Sort/BubbleSort', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.unsortedArray),
+        });
+        const array = await response.json();
+        this.setState({ array: array, sorted: true, sorting: false });
+    }
+
+    async selectionSort() {
+        this.setState({ array: this.unsortedArray, sorted: false, sorting: true });
+        const response = await fetch('Sort/SelectionSort', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.unsortedArray),
+        });
+        const array = await response.json();
+        this.setState({ array: array, sorted: true, sorting: false });
+    }
+
+    static renderArray(array) {
+        if (array.length > 25) {
+            return (
+                <h4>Loaded array but it's too big to show. Check console</h4>
+            );
+        }
+        var array1 = array.toString();
+        return (
+            <h4>{array1}</h4>
+                );
     }
 }
--- SortingAlgorithms/Controllers/SortController.cs
+++ SortingAlgorithms/Controllers/SortController.cs
@@ -21,11 +21,41 @@ namespace SortingAlgorithms.Controllers
             _logger = logger;
         }
 
-        [HttpPost]
-        public IEnumerable<int> Get(string size)
+        [HttpPost][Route("Generate")]
+        public IEnumerable<int> Get([FromBody]string size)
         {
             System.IO.File.WriteAllText(@"C:\Users\Kamil\Desktop\WriteText.txt", size);
-            return Models.GenerateUnsortedArray.GenerateArray(20);
+            return Models.GenerateUnsortedArray.GenerateArray(Int32.Parse(size));
+        }
+
+        [HttpPost]
+        [Route("QuickSort")]
+        public IEnumerable<int> QuickSort([FromBody]int[] array)
+        {
+            System.IO.File.WriteAllText(@"C:\Users\Kamil\Desktop\WriteText.txt", array.ToString());
+
+            return Models.QuickSort.Sort(array);
+        }
+
+        [HttpPost]
+        [Route("MergeSort")]
+        public IEnumerable<int> MergeSort([FromBody]int[] array)
+        {
+            return Models.MergeSort.Sort(array);
+        }
+
+        [HttpPost]
+        [Route("BubbleSort")]
+        public IEnumerable<int> BubbleSort([FromBody]int[] array)
+        {
+            return Models.BubbleSort.Sort(array);
+        }
+
+        [HttpPost]
+        [Route("SelectionSort")]
+        public IEnumerable<int> SelectionSort([FromBody]int[] array)
+        {
+            return Models.SelectionSort.Sort(array);
         }
     }
 }
