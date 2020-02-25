3a47ca22e77b22b304b098d09063fbb61a05b0be
Kamil <tret1lus@gmail.com>
2020-02-25 16:12:47 +0100

    added algorithms description

--- SortingAlgorithms/ClientApp/src/components/Home.js
+++ SortingAlgorithms/ClientApp/src/components/Home.js
@@ -17,22 +17,30 @@ export class Home extends Component {
                 <tbody>
                     <tr>
                         <td>Bubble Sort</td>
-                        <td></td>
+                        <td>Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.&nbsp;
+                            <a href="https://www.geeksforgeeks.org/bubble-sort/">Source</a>
+                        </td>
                         <td><b>O(N^2)</b></td>
                     </tr>
                     <tr>
                         <td>Selection Sort</td>
-                        <td></td>
+                        <td>The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning.&nbsp;
+                            <a href="https://www.geeksforgeeks.org/selection-sort/">Source</a>
+                            </td>
                         <td><b>O(N^2)</b></td>
                     </tr>
                     <tr>
                         <td>Merge Sort</td>
-                        <td></td>
+                        <td>Merge sort works on the principle of Divide and Conquer. Merge sort repeatedly breaks down a list into several sublists until each sublist consists of a single element and merging those sublists in a manner that results into a sorted list.&nbsp;
+                            <a href="https://www.interviewbit.com/tutorial/merge-sort-algorithm/">Source</a>
+                            </td>
                         <td><b>&Theta;(nLogn)</b></td>
                     </tr>
                     <tr>
                         <td>Quick Sort</td>
-                        <td></td>
+                        <td>QuickSort is one of the most efficient sorting algorithms and is based on the splitting of an array into smaller ones. The name comes from the fact that, quick sort is capable of sorting a list of data elements significantly faster than any of the common sorting algorithms. And like Merge sort, Quick sort also falls into the category of divide and conquer approach of problem-solving methodology.&nbsp; 
+                            <a href="https://www.interviewbit.com/tutorial/quicksort-algorithm/">Source</a>
+                            </td>
                         <td><b>&Theta;(nLogn)</b></td>
                     </tr>
                 </tbody>
